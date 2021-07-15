0a64a742d89e95bb0f42e10f265e7d17c9732d9e
Calvin Dsouza <calvin091518dsouza@gmail.com>
2021-07-15 23:14:27 +0530

    Beginning Files

---
+++ src/pages/HabitPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HabitPage = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default HabitPage
