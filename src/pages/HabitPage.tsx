5498573246052548d440776935c8d6c3e9a33e06
Calvin Dsouza <37261038+kalzen15@users.noreply.github.com>
2021-08-27 23:00:14 +0530

    Created button config using paper

--- src/pages/HabitPage.tsx
+++ src/pages/HabitPage.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const HabitPage = () => {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
 
-export default HabitPage
+export default HabitPage;
