0ed3c1657a5a7a50651a44fb4184ecbaa7ea2e01
Calvin Dsouza <37261038+kalzen15@users.noreply.github.com>
2021-08-28 23:06:10 +0530

    Created theme abstraction

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Provider } from "react-native-paper";
 import Button from "./src/components/Button";
+import Header from "./src/components/Header";
+import TextInput from "./src/components/TextInput";
 import { theme } from "./src/core/theme";
 
 export default function App() {
   return (
     <Provider theme={theme}>
       <View style={styles.container}>
-        <Text>Hello</Text>
+        <Header>Hello</Header>
         <Button mode="contained">Click Here</Button>
+        <TextInput label="Email" />
       </View>
     </Provider>
   );
